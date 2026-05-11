caac29db9fd85d688dac3933d4596072065e00f5
Elias Matin <elias.matin@allianz.de>
2026-05-11 14:53:46 +0200

    Add Bean Validation to entities

--- src/main/java/macros/macros/model/Meal.java
+++ src/main/java/macros/macros/model/Meal.java
@@ -1,6 +1,7 @@
 package macros.macros.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,11 +18,22 @@ public class Meal {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Min(value = 0, message = "Calories must be positive")
     private int calories;
+
+    @Min(value = 0, message = "Protein must be positive")
     private double protein;
+
+    @Min(value = 0, message = "Fat must be positive")
     private double fat;
+
+    @Min(value = 0, message = "Carbs must be positive")
     private double carbs;
+
+    @NotNull(message = "Date is required")
     private LocalDate date;
 
     @ManyToOne
--- src/main/java/macros/macros/model/User.java
+++ src/main/java/macros/macros/model/User.java
@@ -1,6 +1,7 @@
 package macros.macros.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,14 +19,28 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @Min(value = 1, message = "Weight must be positive")
     private double weight;
+
+    @Min(value = 1, message = "Height must be positive")
     private double height;
+
+    @Min(value = 1, message = "Age must be positive")
     private int age;
+
     private boolean male;
+
+    @Min(value = 1, message = "PAL value must be at least 1")
     private double palValue;
-    private String goal; // lose, maintain, gain
-    private String city; // for weather api
+
+    @NotBlank(message = "Goal is required")
+    private String goal; // LOSE, MAINTAIN, GAIN
+
+    @NotBlank(message = "City is required")
+    private String city;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Meal> meals;
