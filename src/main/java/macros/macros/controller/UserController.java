d4501a7f99c7c17a14254e6463b04d4f0646eebe
Elias Matin <elias.matin@allianz.de>
2026-05-11 11:08:51 +0200

    Add UserController with calorie endpoint

---
+++ src/main/java/macros/macros/controller/UserController.java
@@ -0,0 +1,48 @@
+package macros.macros.controller;
+
+import macros.macros.model.User;
+import macros.macros.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/{id}/calories")
+    public double getDailyCalories(@PathVariable Long id) {
+        return userService.calculateDailyCalories(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
